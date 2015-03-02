fe823b62d5cb3e81582d0532876e5e972201c6a1
Benjamine89 <benjamine89@gmail.com>
2015-03-02 15:11:10 +0100

    app-fullscreen virker. app-redirect from browser to webview virker. app-tilbage knap virker.

---
+++ app/src/main/java/tranumnet/dk/tranumapp/MyAppWebViewClient.java
@@ -0,0 +1,23 @@
+package tranumnet.dk.tranumapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Benjamin on 28-02-2015.
+ */
+public class MyAppWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if(Uri.parse(url).getHost().endsWith("tranumnet.dk")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
