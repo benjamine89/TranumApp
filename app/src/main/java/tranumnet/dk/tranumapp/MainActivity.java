5b6b97733a63a42a3c346b5ee9d5fea905c10170
Benjamine89 <benjamine89@gmail.com>
2015-03-24 17:22:18 +0100

    når der startes og ingen forbindelse vises der en toast med ingen forbindelse.

--- app/src/main/java/tranumnet/dk/tranumapp/MainActivity.java
+++ app/src/main/java/tranumnet/dk/tranumapp/MainActivity.java
@@ -6,50 +6,90 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity
+{
 
     private WebView mWebView;
+    private String myToast = "Der er ingen internet forbindelse i øjeblikket, venlist prøv igen senere.";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // mWebView tilgår WebView fra activity_main
+        //gør fullscreen på app
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // Viser layout activity_main når app starter
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
 
-        // Tilslutning af Javascript
 
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+
+
+        // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setSaveFormData(true);
 
-        // Tilgå tranumnet gennem URL'en
+        //bruger denne url til at starte i webview
         mWebView.loadUrl("http://www.tranumnet.dk");
 
-        // Stop lokale links og omdiriger fra åben browser istedet brug WebView
+        // Stop local links and redirects from opening in browser instead of WebView
         mWebView.setWebViewClient(new MyAppWebViewClient());
+
+
+
+        if ( isNetworkAvailable())
+        {
+            //gør synlig når webview virker
+            mWebView.setVisibility(View.VISIBLE);
+
+        }
+        else
+        {
+            //toast ved ingen forbindelse 2 gange for længere tid vist (ca. 3,5 sek pr stk. i alt 7 sek).
+            Toast.makeText(this, myToast, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, myToast,Toast.LENGTH_LONG).show();
+        }
+
+
 
     }
 
     @Override
-    public void onBackPressed()
+    public boolean onKeyDown (int keyCode, KeyEvent event)
     {
-        if (mWebView.canGoBack())
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack())
         {
             mWebView.goBack();
-        } else
-        {
-            super.onBackPressed();
+            return true;
         }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    void Showtoast(String message)
+    {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
     }
 
 
@@ -65,10 +105,27 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        //opdater knap i menu vil opdatere.
+        switch (item.getItemId())
+        {
+            case R.id.action_update:
+                mWebView.reload();
+                break;
+        }
+        //afslut app knap
+        switch (item.getItemId())
+        {
+            case R.id.action_quit:
+                finish();
+                break;
+        }
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_update)
+        {
             return true;
         }
 
@@ -92,13 +149,28 @@ public class MainActivity extends Activity {
     }
 
 
-    public boolean isOnline()
+    //bruges til at skrive om enhed er online hvis den ikke er.
+    private boolean isNetworkAvailable()
     {
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        return netInfo != null && netInfo.isConnectedOrConnecting();
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            isAvailable = true;
+        }
+
+
+        //kode til at gemme i lokal cache
+        mWebView.getSettings().setAppCacheMaxSize(1024*1024*8);
+        mWebView.getSettings().setAppCachePath(("/data/data/"+ getPackageName() +"/cache"));
+        mWebView.getSettings().setAppCacheEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setAllowFileAccess(true);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        return isAvailable;
     }
 }
 
--- app/src/main/java/tranumnet/dk/tranumapp/MyAppWebViewClient.java
+++ app/src/main/java/tranumnet/dk/tranumapp/MyAppWebViewClient.java
@@ -1,23 +1,37 @@
 package tranumnet.dk.tranumapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 /**
  * Created by Benjamin on 28-02-2015.
  */
-public class MyAppWebViewClient extends WebViewClient {
+public class MyAppWebViewClient extends WebViewClient
+{
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if(Uri.parse(url).getHost().endsWith("tranumnet.dk")) {
+    public boolean shouldOverrideUrlLoading(WebView view, String url)
+    {
+        //parser url tranumnet.dk og viser i webview men kun url's som er under tranumnet.dk
+        if(Uri.parse(url).getHost().endsWith("tranumnet.dk"))
+        {
             return false;
         }
 
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         view.getContext().startActivity(intent);
+
+
         return true;
+
     }
+
+
 }
