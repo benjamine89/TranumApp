05681cbefe66d543be0bac10fc64c1b296a4036d
Benjamine89 <benjamine89@gmail.com>
2015-03-25 10:42:51 +0100

    fragment slettet

--- app/src/main/java/tranumnet/dk/tranumapp/MainActivity.java
+++ app/src/main/java/tranumnet/dk/tranumapp/MainActivity.java
@@ -4,14 +4,11 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
@@ -123,21 +120,6 @@ public class MainActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public static class PlaceholderFragment extends Fragment
-    {
-
-        public PlaceholderFragment()
-        {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState)
-        {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
     }
 
 
